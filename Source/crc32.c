420923d02c9877ef5b3f2b29c1fb8b31f6a6fdee
Roy Ratcliffe <roy@ratcliffe.me>
2023-02-22 20:18:07 +0000

    docs: duplicated

--- Source/crc32.c
+++ Source/crc32.c
@@ -1,18 +1,5 @@
 /*!
  * \file crc32.c
- *
- * Defines an abstract family of CRC peripheral operation functions. Imagine a
- * high-speed CRC computing peripheral that computes arbitrary CRC sums given
- * spans of bytes, half-words or words. The peripheral works by first setting up
- * the initial value, polynomial and bit reversing requirements. Subsequently,
- * use by sending a data pointer with a data length in bytes. The peripheral
- * returns a CRC result.
- *
- * The CRC family of STM32 functions listed below present a very thin wrapping
- * around the HAL functions. They attempt to hide the HAL internal type
- * definitions and manifest constants as part of the private implementation. The
- * interface itself does not expose the HAL header. HAL error codes become pass
- * and fail, and all the FreeRTOS-oriented generic types and constants.
  */
 
 #include "crc32.h"
