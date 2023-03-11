b83e4079999057548d32c0422bf7794c1d051401
Roy Ratcliffe <roy@ratcliffe.me>
2023-03-11 14:46:35 +0000

    refactor: rename to stm32xx_crc

--- Drivers/STM32xx/FreeRTOS/Src/stm32xx_crc.c
+++ Drivers/STM32xx/FreeRTOS/Src/stm32xx_crc.c
@@ -1,5 +1,5 @@
 /*!
- * \file crc32_stm32.c
+ * \file stm32xx_crc.c
  *
  * The CRC family of STM32 functions listed below present a very thin wrapping
  * around the HAL functions. They attempt to hide the HAL internal type
