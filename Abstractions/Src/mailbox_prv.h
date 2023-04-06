fa634d64facff5b6def31e31406e57108a6f7f81
Roy Ratcliffe <roy@ratcliffe.me>
2023-04-06 08:48:35 +0100

    refactor: remove semaphore

--- Abstractions/Src/mailbox_prv.h
+++ Abstractions/Src/mailbox_prv.h
@@ -8,10 +8,8 @@
 
 #include "FreeRTOS.h"
 #include "message_buffer.h"
-#include "semphr.h"
 
 struct Mailbox {
   MessageBufferHandle_t xMessageBuffer;
   TaskHandle_t xTask;
-  SemaphoreHandle_t xSemaphore;
 };
