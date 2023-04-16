d666ad6e22f1a506cd7eb9f8c31e6dc10d41c125
Roy Ratcliffe <roy@ratcliffe.me>
2023-04-16 09:33:06 +0100

    fix: list yield pxNextEntry

--- Abstractions/Src/list_ex.c
+++ Abstractions/Src/list_ex.c
@@ -30,18 +30,21 @@ ListItem_t *pxListInsertNewEnd(List_t *pxList, void *pxOwner, TickType_t xValue)
 }
 
 const ListItem_t *pxListYield(List_t *const pxList, BaseType_t (*pxYield)(void *pxOwner, TickType_t xValue)) {
-  const ListItem_t *pxListEnd = listGET_END_MARKER(pxList), *pxListItem = listGET_HEAD_ENTRY(pxList);
-  while (pxListItem != pxListEnd) {
-    if (pxYield(listGET_LIST_ITEM_OWNER(pxListItem), listGET_LIST_ITEM_VALUE(pxListItem)) == pdPASS) return pxListItem;
-    pxListItem = listGET_NEXT(pxListItem);
+  for (const ListItem_t *pxHeadEntry = listGET_HEAD_ENTRY(pxList), *pxEndMarker = listGET_END_MARKER(pxList),
+                        *pxNextEntry;
+       pxHeadEntry != pxEndMarker; pxHeadEntry = pxNextEntry) {
+    pxNextEntry = listGET_NEXT(pxHeadEntry);
+    if (pxYield(listGET_LIST_ITEM_OWNER(pxHeadEntry), listGET_LIST_ITEM_VALUE(pxHeadEntry)) == pdPASS)
+      return pxHeadEntry;
   }
   return NULL;
 }
 
 void vListYield(List_t *const pxList, BaseType_t (*pxYield)(void *pxOwner, TickType_t xValue)) {
-  const ListItem_t *pxListEnd = listGET_END_MARKER(pxList), *pxListItem = listGET_HEAD_ENTRY(pxList);
-  while (pxListItem != pxListEnd) {
-    if (pxYield(listGET_LIST_ITEM_OWNER(pxListItem), listGET_LIST_ITEM_VALUE(pxListItem)) == pdFAIL) return;
-    pxListItem = listGET_NEXT(pxListItem);
+  for (const ListItem_t *pxHeadEntry = listGET_HEAD_ENTRY(pxList), *pxEndMarker = listGET_END_MARKER(pxList),
+                        *pxNextEntry;
+       pxHeadEntry != pxEndMarker; pxHeadEntry = pxNextEntry) {
+    pxNextEntry = listGET_NEXT(pxHeadEntry);
+    if (pxYield(listGET_LIST_ITEM_OWNER(pxHeadEntry), listGET_LIST_ITEM_VALUE(pxHeadEntry)) == pdFAIL) return;
   }
 }
