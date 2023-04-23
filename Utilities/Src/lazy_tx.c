a3551fcb54de1f79b2969d18720c4af43cc0a27c
Roy Ratcliffe <roy@ratcliffe.me>
2023-04-23 08:09:58 +0100

    fix: lazytxDELAY_TICKS semantics

--- Utilities/Src/lazy_tx.c
+++ Utilities/Src/lazy_tx.c
@@ -1,26 +1,5 @@
-/*
- * lazy_tx_uart.c
- * Copyright (c) 2023, Roy Ratcliffe, Northumberland, United Kingdom
- *
- * Permission is hereby granted, free of charge,  to any person obtaining a
- * copy  of  this  software  and    associated   documentation  files  (the
- * "Software"), to deal in  the   Software  without  restriction, including
- * without limitation the rights to  use,   copy,  modify,  merge, publish,
- * distribute, sub-license, and/or sell  copies  of   the  Software,  and to
- * permit persons to whom the Software is   furnished  to do so, subject to
- * the following conditions:
- *
- *     The above copyright notice and this permission notice shall be
- *     included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT  WARRANTY OF ANY KIND, EXPRESS
- * OR  IMPLIED,  INCLUDING  BUT  NOT   LIMITED    TO   THE   WARRANTIES  OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR   PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS  OR   COPYRIGHT  HOLDERS BE LIABLE FOR ANY
- * CLAIM, DAMAGES OR OTHER LIABILITY,  WHETHER   IN  AN ACTION OF CONTRACT,
- * TORT OR OTHERWISE, ARISING FROM,  OUT  OF   OR  IN  CONNECTION  WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
+// Copyright (c) Roy Ratcliffe, Northumberland, United Kingdom
+// SPDX-License-Identifier: MIT
 
 #include "lazy_tx.h"
 
@@ -45,7 +24,7 @@ static portTASK_FUNCTION(prvLazyTxTask, pvParameters) {
     uint8_t ucTxData[lazytxBUFFER_LENGTH_BYTES];
     size_t xBufferLengthBytes =
         xStreamBufferReceive(xLazyTx->xStreamBuffer, ucTxData, sizeof(ucTxData), lazytxDELAY_TICKS);
-    if (xBufferLengthBytes > 0UL) { (*xLazyTx->Handler)(xLazyTx->pvSender, ucTxData, xBufferLengthBytes); }
+    (*xLazyTx->Handler)(xLazyTx->pvSender, xBufferLengthBytes ? ucTxData : NULL, xBufferLengthBytes);
   }
 }
 
