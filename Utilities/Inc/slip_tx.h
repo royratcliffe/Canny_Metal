0ec831fd8813b1088cdad9bcd9601b11cc9c1df1
Roy Ratcliffe <roy@ratcliffe.me>
2023-04-25 18:13:24 +0100

    feat: slip_tx, rx; message_buffer_ex

---
+++ Utilities/Inc/slip_tx.h
@@ -0,0 +1,50 @@
+/*
+ * slip_tx.h
+ */
+
+#pragma once
+
+#include "slip.h"
+
+#include "tx_handler.h"
+
+#ifndef sliptxMAX_PACKET_LEN
+#ifndef slipMAX_PACKET_LEN
+#define sliptxMAX_PACKET_LEN 256U
+#else
+#define sliptxMAX_PACKET_LEN (slipMAX_PACKET_LEN)
+#endif
+#endif
+
+#ifndef sliptxSTACK_DEPTH
+#define sliptxSTACK_DEPTH (configMINIMAL_STACK_SIZE + sliptxMAX_PACKET_LEN)
+#endif
+
+#ifndef sliptxPRIORITY
+#define sliptxPRIORITY 16U
+#endif
+
+/*
+ * \brief Handle of SLIP transmitter.
+ *
+ * The transmitter encapsulates two things: a message buffer on which the
+ * transmitter blocks and a transmitter function with its opaque parameter. The
+ * SLIP transmitter eats incoming un-encoded raw frames and spits out a
+ * SLIP-encoded byte stream.
+ */
+typedef struct SLIPTx *SLIPTxHandle_t;
+
+typedef TxHandler_t SLIPTxSendHandler_t;
+
+SLIPTxHandle_t xSLIPTxCreate(size_t xBufferSizeBytes);
+
+void vSLIPTxSendHandler(SLIPTxHandle_t xSLIPTx, void *pvSend, SLIPTxSendHandler_t xHandler);
+
+/*
+ * \brief Sends raw data from the SLIP transmitter.
+ *
+ * Used privately but also exposed for diagnostic purposes, bypassing the SLIP encoder.
+ */
+void vSLIPTxSend(SLIPTxHandle_t xSLIPTx, const void *pvTxData, size_t xDataLengthBytes);
+
+void vSLIPTxDelete(SLIPTxHandle_t xSLIPTx);
