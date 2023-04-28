54b14d79b3e870fc6e14fcf988f13247ea734d67
Roy Ratcliffe <roy@ratcliffe.me>
2023-04-28 19:09:12 +0100

    style: xMsgUnifier

--- Abstractions/Src/msg_ext_task.c
+++ Abstractions/Src/msg_ext_task.c
@@ -9,10 +9,10 @@ BaseType_t xMsgBindTask(MsgBindingHandle_t xMsgBinding, TaskHandle_t xTask) {
   return xMsgBindExtWithBody(xMsgBinding, msgEXT_TYPE_TASK, &xTask, sizeof(xTask));
 }
 
-BaseType_t xMsgUnifyTask(MsgUnifierHandle_t xMsgUnpack, TaskHandle_t *pxTask) {
+BaseType_t xMsgUnifyTask(MsgUnifierHandle_t xMsgUnifier, TaskHandle_t *pxTask) {
   const void *pvExt;
   size_t xExtLengthBytes;
-  if (!xMsgUnifyExtType(xMsgUnpack, msgEXT_TYPE_TASK, &pvExt, &xExtLengthBytes) || xExtLengthBytes != sizeof(*pxTask))
+  if (!xMsgUnifyExtType(xMsgUnifier, msgEXT_TYPE_TASK, &pvExt, &xExtLengthBytes) || xExtLengthBytes != sizeof(*pxTask))
     return pdFAIL;
   if (pxTask) (void)memcpy(pxTask, pvExt, sizeof(*pxTask));
   return pdPASS;
