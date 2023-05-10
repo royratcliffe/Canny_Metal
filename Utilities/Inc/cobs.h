9824831ed16ad56657933688b87e8d802c457664
Roy Ratcliffe <roy@ratcliffe.me>
2023-05-10 20:24:54 +0100

    feat: COBS encode, decode

---
+++ Utilities/Inc/cobs.h
@@ -0,0 +1,29 @@
+// Copyright (c) Roy Ratcliffe, Northumberland, United Kingdom
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <stdlib.h>
+
+/*!
+ * Attempts to make the encoding process more efficient by fast scanning using
+ * \c memchr and \c memcpy.
+ *
+ * \note Makes use of void pointer arithmetic.
+ */
+size_t xCOBSEncode(const void *pvData, size_t xDataLengthBytes, void *pvCOBS);
+
+/*!
+ * \param[in] pvCOBS Encoded bytes including its trailing null.
+ *
+ * \param[out] pvData Decoded bytes exluding the terminating null.
+ *
+ * \returns Number of decoded bytes. Answers \c 0 if invalid. Never answers zero
+ * when valid. Valid data always terminates with a null and contains no other
+ * nulls.
+ *
+ * Performs validation. All the incoming bytes must be nulls. In the case of an
+ * invalid COBS sequence, the buffer may mutate for partial results if \c pvData
+ * is not \c NULL.
+ */
+size_t xCOBSDecode(const void *pvCOBS, size_t xCOBSLengthBytes, void *pvData);
---
+++ Utilities/Src/cobs.c
@@ -0,0 +1,52 @@
+// Copyright (c) Roy Ratcliffe, Northumberland, United Kingdom
+// SPDX-License-Identifier: MIT
+
+#include "cobs.h"
+
+#include <stdint.h>
+#include <memory.h>
+
+size_t xCOBSEncode(const void *pvData, size_t xDataLengthBytes, void *pvCOBS) {
+  size_t xCOBSLengthBytes = 0UL;
+  while (xDataLengthBytes) {
+    uint8_t uc = xDataLengthBytes < 0xffU ? xDataLengthBytes : 0xfeU;
+    void *pv = memchr(pvData, 0x00U, uc);
+    if (pv) uc = pv - pvData;
+    *(uint8_t *)pvCOBS++ = uc + 0x01U;
+    xCOBSLengthBytes++;
+    if (uc) {
+      if (pvCOBS) {
+        (void)memcpy(pvCOBS, pvData, uc);
+        pvCOBS += uc;
+      }
+      pvData += uc;
+      xCOBSLengthBytes += uc;
+      xDataLengthBytes -= uc;
+    } else {
+      pvData++;
+      xDataLengthBytes--;
+    }
+  }
+  if (pvCOBS) *(uint8_t *)pvCOBS = 0x00U;
+  return xCOBSLengthBytes + 1UL;
+}
+
+size_t xCOBSDecode(const void *pvCOBS, size_t xCOBSLengthBytes, void *pvData) {
+  size_t xDataLengthBytes = 0UL;
+  while (xCOBSLengthBytes > 1UL) {
+    uint8_t uc = *(const uint8_t *)pvCOBS++;
+    if (uc == 0x00U || --uc >= xCOBSLengthBytes--) return 0UL;
+    if (memchr(pvCOBS, 0x00U, uc)) return 0UL;
+    if (uc) {
+      if (pvData) {
+        (void)memcpy(pvData, pvCOBS, uc);
+        pvData += uc;
+      }
+      pvCOBS += uc;
+      xCOBSLengthBytes -= uc;
+      xDataLengthBytes += uc;
+    }
+    if (xCOBSLengthBytes == 0UL) return 0UL;
+  }
+  return *(const uint8_t *)pvCOBS == 0x00U ? xDataLengthBytes : 0UL;
+}
