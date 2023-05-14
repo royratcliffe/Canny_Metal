f4b73c641617dbe620a4f8c9403a756079413ddf
Roy Ratcliffe <roy@ratcliffe.me>
2023-05-14 06:35:38 +0100

    fix: xCOBSStuff, xCOBSMemStuff

--- Utilities/Inc/cobs.h
+++ Utilities/Inc/cobs.h
@@ -5,13 +5,23 @@
 
 #include <stdlib.h>
 
+ptrdiff_t xCOBSStuff(const void *pvData, size_t xDataLengthBytes, void *pvCOBS);
+
 /*!
  * Attempts to make the encoding process more efficient by fast scanning using
  * \c memchr and \c memcpy.
  *
  * \note Makes use of void pointer arithmetic.
  */
-size_t xCOBSStuff(const void *pvData, size_t xDataLengthBytes, void *pvCOBS);
+size_t xCOBSMemStuff(const void *pvData, size_t xDataLengthBytes, void *pvCOBS);
+
+/*!
+ * Un-stuffing checks the stuff spans for non-zero content.
+ *
+ * Spans of 255 have no zero terminator. Stuff code 0xff does \e not carry an
+ * implied zero.
+ */
+ptrdiff_t xCOBSUnStuff(const void *pvCOBS, size_t xCOBSLengthBytes, void *pvData);
 
 /*!
  * \param[in] pvCOBS Encoded bytes including its trailing null.
@@ -26,4 +36,4 @@ size_t xCOBSStuff(const void *pvData, size_t xDataLengthBytes, void *pvCOBS);
  * invalid COBS sequence, the buffer may mutate for partial results if \c pvData
  * is not \c NULL.
  */
-size_t xCOBSUnStuff(const void *pvCOBS, size_t xCOBSLengthBytes, void *pvData);
+size_t xCOBSMemUnStuff(const void *pvCOBS, size_t xCOBSLengthBytes, void *pvData);
