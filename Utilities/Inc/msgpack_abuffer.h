d2ef25e38f84a91b4de6e90908819af934616176
Roy Ratcliffe <roy@ratcliffe.me>
2023-05-14 06:31:04 +0100

    docs: refactor

--- Utilities/Inc/msgpack_abuffer.h
+++ Utilities/Inc/msgpack_abuffer.h
@@ -1,6 +1,5 @@
-/*
- * msgpack_abuffer.h
- */
+// Copyright (c) Roy Ratcliffe, Northumberland, United Kingdom
+// SPDX-License-Identifier: MIT
 
 #pragma once
 
@@ -31,12 +30,33 @@ struct msgpack_abuffer {
 
 typedef struct msgpack_abuffer msgpack_abuffer_t;
 
+/*!
+ * \brief Initialises a buffer for message packing.
+ * \param[in] data Mutable buffer. It must remain allocated, e.g. on the stack,
+ * while packing.
+ *
+ * \note There is no release interface by design; the buffer structure and the
+ * memory it manages must be either static or stack-allocated.
+ */
 void msgpack_abuffer_init(msgpack_abuffer_t *abuf, msgpack_abuffer_data_t *data, size_t max_size);
 
+/*!
+ * \brief Writes data to buffer.
+ * \param[in] data Pointer to \c msgpack_abuffer cast to void.
+ * \returns \c 0 for success; \c -1 for failure.
+ *
+ * Implements the buffer-write interface for a message packer.
+ */
 int msgpack_abuffer_write(void *data, const msgpack_abuffer_data_t *buf, size_t len);
 
 void msgpack_abuffer_clear(msgpack_abuffer_t *abuf);
 
+/*!
+ * \brief Answers the current size of the buffer.
+ */
 size_t msgpack_abuffer_size(const msgpack_abuffer_t *abuf);
 
+/*!
+ * \brief Answers the start of the buffered data.
+ */
 const msgpack_abuffer_data_t *msgpack_abuffer_data(const msgpack_abuffer_t *abuf);
