a05479f0239dc861ded0aa9437a39df6ffb4ca2f
Roy Ratcliffe <roy@ratcliffe.me>
2026-03-16 06:24:37 +0000

    Refactor clock64 documentation and improve clarity
    Update the documentation for clock64_src and clock64_now functions
    to provide clearer descriptions of their behaviour and usage. This
    includes details on how the source clock is retrieved and the
    conditions under which the current time is determined.

--- Abstractions/Inc/clock64.h
+++ Abstractions/Inc/clock64.h
@@ -49,16 +49,6 @@ struct clock64_impl {
  * \param clock The clock64 instance to access.
  * \retval A pointer to the source clock of the given clock64 instance.
  * \retval NULL if the clock64 instance does not have a source clock.
- * \details This function retrieves the source clock associated with a given
- * clock64 instance by accessing the \c car field of the cons cell contained
- * within the clock64 structure.
- *
- * The source clock is stored in the \c car field of the cons
- * cell; this function returns a pointer to it. If the \c car field is NULL,
- * it indicates that there is no source clock associated with the clock; the
- * function will return NULL. This design allows for a flexible association of a
- * clock64 instance with any type of source clock, as the \c car field can hold
- * a pointer to any data type.
  */
 struct clock64 *clock64_src(struct clock64 *clock);
 
@@ -66,6 +56,11 @@ struct clock64 *clock64_src(struct clock64 *clock);
  * \brief Latches the "current" time from the clock.
  * \param clock The clock64 instance to read the time from.
  * \retval The current time as a 64-bit unsigned integer.
+ * \note If the clock has its own implementation, it will use that to get the
+ * current time. If it does not have its own implementation but has a source
+ * clock, it will use the source clock to get the current time. If neither is
+ * available, it will return 0, indicating that the current time cannot be
+ * determined.
  */
 uint64_t clock64_now(struct clock64 *clock);
 
--- Abstractions/Src/clock64.c
+++ Abstractions/Src/clock64.c
@@ -4,6 +4,14 @@
 
 static struct clock64 *clock_of_node(struct cons_node *node) { return containerof(node, struct clock64, node); }
 
+/*
+ * Retrieves the source clock associated with a given clock64 instance by
+ * accessing the car field of the cons cell contained within the clock64
+ * structure. If the car field is NULL, it indicates that there is no source
+ * clock associated with the clock; the function will return NULL. This design
+ * allows for a flexible association of a clock64 instance with any source
+ * clock, as the car field can hold a pointer to any data type.
+ */
 struct clock64 *clock64_src(struct clock64 *clock) { return clock_of_node(cons_car_node(&clock->node)); }
 
 uint64_t clock64_now(struct clock64 *clock) { return clock->impl->now(); }
@@ -38,6 +46,15 @@ void clock64_tick(struct clock64 *clock) {
 }
 
 void clock64_sync(struct clock64 *clock) {
+  /*
+   * Latch the current time from the clock and update the clock's ticks if it
+   * has changed since the last tick. If the time has changed, call the clock's
+   * tick function and then synchronise all sub-clocks (sub-nodes) of the clock.
+   * This ensures that all clocks in the hierarchy are synchronised to the same
+   * time and that any necessary updates are performed when the time changes. If
+   * the time has not changed, there is no need to synchronise the sub-clocks,
+   * as they will already be in sync with the current time.
+   */
   uint64_t ticks = clock64_now(clock);
   if (clock->ticks != ticks) {
     clock->ticks = ticks;
