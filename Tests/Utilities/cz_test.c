2b500cc9c9b92b8acdd005de5898e5333634edef
Roy Ratcliffe <roy@ratcliffe.me>
2026-05-10 18:33:11 +0100

    Add test for lsb32 function in cz_test.c
    Include unit tests for the lsb32 function to ensure correct
    functionality and expected outputs.

--- Tests/Utilities/cz_test.c
+++ Tests/Utilities/cz_test.c
@@ -13,10 +13,19 @@ void test_lszb32(void) {
   assert(0x00000008U == lszb32(0x00000007U));
 }
 
+void test_lsb32(void) {
+  assert(0x00000000U == lsb32(0x00000000U));
+  assert(0x00000001U == lsb32(0x00000001U));
+  assert(0x00000002U == lsb32(0x00000002U));
+  assert(0x00000001U == lsb32(0x00000003U));
+  assert(0x00000004U == lsb32(0x00000004U));
+}
+
 int main(void) {
   (void)printf("Hello, World from %s!!!\n", "cz_test");
 
   test_lszb32();
+  test_lsb32();
 
   _exit(0);
   return 0;
